7b4a422443713157ef24836d4e366e9c1a154763
Farhan Farooq <md.farhan.farooq@g.bracu.ac.bd>
2021-11-19 10:24:38 +0600

    .

---
+++ demo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    a = 12;
+    cout<<"size of a "<<sizeof(a)<<endl;
+    return 0;
+}
---
+++ dowhile_loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int sum = 0;
+    do{
+        cout << n << endl;
+        cin >> n;
+    }while(n>0);
+    return 0;
+}
