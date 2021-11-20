02a0b0920fee50040e2bb4e2fdd25ab361f1feca
Farhan Farooq <md.farhan.farooq@g.bracu.ac.bd>
2021-11-20 12:33:10 +0600

    .

---
+++ codeforces/Watermelon.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+        if(n>2 && n%2==0){
+            cout << "Yes" << endl;
+        }
+        else{
+            cout << "No" << endl;
+        }
+    return 0;
+}
